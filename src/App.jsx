d728397d14cbd857a0f50c2a1914e0d318729fda
djurakulov14 <109537648+djurakulov14@users.noreply.github.com>
2023-01-11 22:24:10 +0500

    added smth

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import './App.css';
+import './App.scss';
 
 function App() {
   const tours = useSelector(state => state.tours.tours)
---
+++ src/Components/InfoPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const InfoPage = () => {
+  return (
+    <div className='container'>
+        
+    </div>
+  )
+}
