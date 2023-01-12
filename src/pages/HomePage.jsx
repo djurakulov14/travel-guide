96ddd2fbe9fc4b0c9199bae30dfcb60e9694a5f4
jony <sabinalatipova20@gmail.com>
2023-01-12 15:36:43 +0500

    fixed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,7 +8,7 @@ export const HomePage = () => {
   console.log(tours);
 
   return (
-    <div className="px-5 bg-[url('http://www.opts.tours/images/slider/slide3.jpg')]">
+    <div className="px-5 bg-[url('http://www.opts.tours/images/slider/slide3.jpg')] bg-no-repeat bg-cover	bg-center	">
       <section className='container m-auto w-fll flex justify-around px-5 max-md:justify-start gap-20 py-36 text-gray-500 max-xl:py-28 max-lg:py-20 max-md:py-14 max-md:gap-10 max-sm:py-5 max-sm:flex-col'>
         <div className="">
           <img className='max-w-sm max-xl:max-w-xs max-lg:max-w-[270px] max-md:max-w-[200px] max-sm:max-w-full' src={user} alt="" />
