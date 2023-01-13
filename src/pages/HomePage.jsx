4a50fb6be07c9170f2e5d937dde9cd427aa20e5d
Shoxrux Code <shoxruxpl04@gmail.com>
2023-01-13 19:52:58 +0500

    added: new things

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,10 +3,15 @@ import { useSelector } from 'react-redux';
 import Cards from '../Components/Cards';
 import user from '../resources/user.jpg'
 import star from '../resources/star.svg'
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+import Questions from '../Components/Questions';
 
 export const HomePage = () => {
   const tours = useSelector(state => state.tours.tours)
-
+  AOS.init({
+    once: true
+})
   return (
     <> 
     <div className="mb-20">
@@ -33,15 +38,37 @@ export const HomePage = () => {
             <p className='text-lg font-normal'>Lorem, ipsum dolor. </p><img src={star} alt="" />
             <p className='text-lg font-normal'>Lorem, ipsum dolor. </p><img src={star} alt="" />
           </div>
-          
         </marquee>
       </div>
       </div>
-      <section className='container m-auto px-5 grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5'>
+      <div className='container px-5 m-auto w-[100%]'>
+      <div className='mb-10'>
+        <h1 data-aos="fade-left"
+         className='text-5xl font-medium'
+         data-aos-offset="300"
+         data-aos-easing="ease-in-sine"
+         data-aos-duration="500">Обо мне</h1>
+        <p data-aos="fade-left"
+         className='mt-3 mb-3'
+         data-aos-offset="300"
+         data-aos-easing="ease-in-sine"
+         data-aos-duration="500"
+         >Меня зовут anonymus и я являюсь действующим гидом по городам Узбекистана. 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptas officiis aspernatur aperiam molestias quidem aliquam maxime nulla quibusdam! Distinctio? Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, aperiam. Voluptatem aut, obcaecati accusantium qui totam sapiente ea consectetur ratione. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime esse neque, reprehenderit et aspernatur repellat explicabo omnis minus enim fuga. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis, placeat dignissimos doloribus itaque modi natus delectus omnis suscipit quidem magnam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime esse neque, reprehenderit et aspernatur repellat explicabo omnis minus enim fuga. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis, placeat dignissimos doloribus itaque modi natus delectus omnis suscipit quidem magnam.
+        </p>
+      </div>
+      <div className='mb-5'>
+        <h1 className='text-5xl font-medium'>Актуальные туры</h1>
+      </div>
+      </div>
+      <section className='container m-auto px-5 grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 mb-10'>
         {
           tours.map(i => <Cards item={i} key={i.id} />)
         }
       </section>
+      <div className='container px-5 m-auto w-[100%]'>
+      <Questions />
+      </div>
     </>
   )
 }
