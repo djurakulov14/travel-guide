625b220d730071dda258710ff28c357ed7223250
djurakulov14 <109537648+djurakulov14@users.noreply.github.com>
2023-01-16 17:56:34 +0500

    added road map

--- src/pages/InfoPage.jsx
+++ src/pages/InfoPage.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { useLocation, useNavigate } from 'react-router-dom'
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
-import { Navigation } from "swiper";
 import { BiLeftArrowAlt } from 'react-icons/bi';
 import AOS from 'aos';
 import kalpok from "../resources/kalpok.png"
 
 export const InfoPage = () => {
+    const takeHeigth = useRef(null)
     window.scrollTo(0, 0);
     let {pathname} = useLocation()
     let splited = pathname.split(':').at(-1)
@@ -21,7 +20,7 @@ export const InfoPage = () => {
     AOS.init({
         once: true
       })
-
+    //  
     return (
         <>
         <section
@@ -29,11 +28,11 @@ export const InfoPage = () => {
         data-aos-offset="300"
         data-aos-easing="ease-in-sine"
         data-aos-duration="500"
-        className="overflow-x-hidden w-full h-[600px] bg-cover bg-center mb-10 max-[680px]:h-[400px] max-[500px]:h-[300px] max-[500px]:mb-0">
-            <div className="div w-full absolute -z-10 h-[600px] max-[680px]:h-[400px] max-[500px]:h-[300px]"><img className='w-full absolute -z-10 h-[600px] max-[680px]:h-[400px] max-[500px]:h-[300px]' src={info.img2} alt="" /></div>
+        className="overflow-hidden w-full bg-cover bg-center mb-10 h-[600px] max-[680px]:h-[400px] max-[500px]:h-[300px] max-[500px]:mb-0 max-[400px]:h-[250px]">
+            <div className="div w-full absolute -z-10 h-[600px] max-[680px]:h-[400px] max-[500px]:h-[300px] max-[400px]:h-[250px]"><img className='w-full absolute -z-10 h-[600px] max-[680px]:h-[400px] max-[500px]:h-[300px] max-[400px]:h-[250px]' src={info.img2} alt="" /></div>
             <div className='m-auto container text-white pt-48 max-[680px]:pt-14 max-[680px]:p-5 max-[500px]:pt-8'>
                 <h1 className='text-5xl font-bold mb-10 max-[680px]:text-3xl max-sm:text-4xl max-[500px]:text-2xl max-[500px]:mt-10'>{info.title}</h1>
-                <p className=" text-4xl font-medium max-[680px]:text-3xl  max-[500px]:text-xl  max-[390px]:text-base" >Изучить главные шедевры архитектуры, услышать истории из прошлого и ощутить сумеречную магию Востока</p>
+                <p className=" text-4xl font-medium max-[680px]:text-3xl max-[500px]:text-xl  max-[390px]:text-base " >Изучить главные шедевры архитектуры, услышать истории из прошлого и ощутить сумеречную магию Востока</p>
                 <BiLeftArrowAlt className=' absolute top-2' onClick={() => navigate(-1)} size={40}/>
             </div>
         </section>
@@ -46,34 +45,23 @@ export const InfoPage = () => {
                     <h1
                     data-aos="fade-right"
                     className=' text-2xl font-bold mb-5'>Что вас ожидает</h1>
-                    <div className="plcesss flex flex-col gap-4 mb-14">
+                    <div ref={takeHeigth} className="plcesss flex flex-col gap-4 mb-14">
+                    <div className="roadline w-[4px] h-full bg-[#3ba4ec] rounded-full absolute z-[-1] left-36"></div>
                     {
                         info.places.map(item =>  
-                        <div key={item.id} className="places w-[95%] max-lg:w-full"
+                        <div key={item.id} className="places w-[95%] max-lg:w-full before:w-4 before:h-4 before:top-2 before:bg-white before:border-[#3ba4ec] before:absolute before:-left-[51px] before:rounded-full before:z-40"
                         data-aos="fade-right"
                         data-aos-offset="300"
                         data-aos-easing="ease-in-sine"
                         data-aos-duration="500"
                         >
-                            <h1 className=' text-lg font-bold'>{item.title}</h1>
+                            <h1 className=' text-xl font-bold'>{item.title}</h1>
                             <div className="infoCont flex gap-2 h-28 max-[700px]:flex-col max-[820px]:gap-0 max-md:block max-md:h-fit">
-                                <p className='text-ellipsis max-lg:mb-3'>{item.body}</p>
+                                <p className='text-ellipsis text-xl max-lg:mb-3'>{item.body}</p>
                                 <img src={item.img} alt={item.title} className=" rounded-xl max-md:w-full" />
                             </div>
                         </div>)
                     }
-                    </div>
-                    <div
-                    data-aos="fade-right"
-                    data-aos-offset="300"
-                    data-aos-easing="ease-in-sine"
-                    data-aos-duration="500"
-                    className="slider w-[95%] max-lg:w-full max-lg:mb-10 mt-5 h-fit bg-[#F2F1EF] ">
-                        <Swiper navigation={true} modules={[Navigation]} className="mySwiper bg-[#F2F1EF] w-[70%] m-auto max-lg:W-[100%]">
-                            {
-                                info.places.map(item => <SwiperSlide key={item.id} className='bg-[#F2F1EF]'><img src={item.img} className=' rounded-lg w-full h-full' alt="" /></SwiperSlide>)
-                            }
-                        </Swiper>
                     </div>
                 </div>
                 <div
@@ -87,18 +75,24 @@ export const InfoPage = () => {
                         <h1 className=' font-bold text-xl mb-5 max-xl:font-semibold'>Индивидуальная экскурсия для 1–4 человек</h1>
                         <div className="duration flex justify-between mb-3">
                             <h1 className=' font-bold max-xl:font-semibold'>Длительность</h1>
-                            <p>3 часа</p>
+                            <p>6 часа</p>
                         </div>
-                        <div className="duration flex justify-between mb-3">
-                            <h1 className=' font-bold max-xl:font-semibold'>Как проходит</h1>
-                            <p>На машине</p>
-                        </div>
-                        <div className="duration flex justify-between mb-3">
-                            <h1 className=' font-bold max-xl:font-semibold'>Дети</h1>
-                            <p>Можно с детьми</p>
+                        <div className="duration flex flex-col justify-between mb-3">
+                            <h1 className=' font-bold max-xl:font-semibold'>В стоимость входит :</h1>
+                            <p>Экскурсия с гидом</p>
+                            <p>Входные билеты</p>
+                            <p>Транспорт во время экскурсии</p>
                         </div>
                     </div>
-                    <div className="bot pt-5">
+                    <div className="bot pt-3">
+                        <div className="prices flex justify-between items-center">
+                        <h1 className='font-bold'>Цена :</h1>
+                            <div className="ps">
+                                <p>1 человек по <b>100$</b></p>
+                                <p>2 человека по <b>65$</b></p>
+                                <p>3 человека по <b>55$</b></p>
+                            </div>
+                        </div>
                         <a href="https://t.me/sard0r_js"><button className='w-full bg-[#00000030] p-3 rounded-lg text-center mt-2'>Забронировать</button></a>
                     </div>
                 </div>
