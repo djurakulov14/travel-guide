3850796e0dbe33c72394322fefb6e0563c2ac610
Shoxrux Code <shoxruxpl04@gmail.com>
2023-01-19 12:54:47 +0500

    fixed errors

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -18,12 +18,12 @@ const Cards = ({ item }) => {
                 <div className="p-5">
                     <h5 className="mb-2 text-2xl max-lg:text-lg font-bold tracking-tight text-dark">{item.title}</h5>
                     <p className=" cont-n mb-3 font-normal text-gray-700 dark:text-gray-400 ">{item.body}</p>
-                    <Link to={`/info/:` + item.id}>
+                    {/* <Link to={`/info/:` + item.id}> */}
                         <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#3ba4ec] rounded-lg hover:bg-[#1f97e7] focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                             {t("moreInfoBtn")}
                             <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                         </button>
-                    </Link>
+                    {/* </Link> */}
                 </div>
             </div>
         </Link>
