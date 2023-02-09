036295cc17434de642deb36c74bcfafda69076e9
Mirsaid25 <siddikovmirsaid01@gmail.com>
2023-02-09 17:36:36 +0500

    questions

--- src/Components/Questions.jsx
+++ src/Components/Questions.jsx
@@ -75,6 +75,106 @@ export default function CustomizedAccordions() {
           </Typography>
         </AccordionDetails>
       </Accordion>
+      <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+        <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+          <Typography>{t("question3")}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          {t("questionAnswer3")}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+        <AccordionSummary aria-controls="panel4d-content" id="panel4d-header">
+          <Typography>{t("question4")}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          {t("questionAnswer4")}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel5'} onChange={handleChange('panel5')}>
+        <AccordionSummary aria-controls="panel5d-content" id="panel5d-header">
+          <Typography>{t("question5")}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          {t("questionAnswer3")}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel6'} onChange={handleChange('panel6')}>
+        <AccordionSummary aria-controls="panel6d-content" id="panel6d-header">
+          <Typography>{t("question6")}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          {t("questionAnswer6")}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel7'} onChange={handleChange('panel7')}>
+        <AccordionSummary aria-controls="panel7d-content" id="panel7d-header">
+          <Typography>{t("question7")}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          {t("questionAnswer7")}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel8'} onChange={handleChange('panel8')}>
+        <AccordionSummary aria-controls="panel8d-content" id="panel8d-header">
+          <Typography>{t("question8")}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          {t("questionAnswer8")}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel9'} onChange={handleChange('panel9')}>
+        <AccordionSummary aria-controls="panel9d-content" id="panel9d-header">
+          <Typography>{t("question9")}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          {t("questionAnswer9")}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel10'} onChange={handleChange('panel10')}>
+        <AccordionSummary aria-controls="panel10d-content" id="panel10d-header">
+          <Typography>{t("question10")}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          {t("questionAnswer10")}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel11'} onChange={handleChange('panel11')}>
+        <AccordionSummary aria-controls="panel11d-content" id="panel11d-header">
+          <Typography>{t("question11")}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          {t("questionAnswer11")}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel12'} onChange={handleChange('panel12')}>
+        <AccordionSummary aria-controls="panel12d-content" id="panel12d-header">
+          <Typography>{t("question12")}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          {t("questionAnswer12")}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
       <div className="m-auto shadow-md flex items-center flex-wrap justify-between px-4 py-3 border border-[#e5e5e5] bg-[#f7f7f7] ">
         <p className="text-[1rem">
           {t("Telega1")}  <span className='max-[465px]:hidden'>{t("Telega2")}</span> {t("Telega3")} <span className='max-[465px]:hidden'>{t("Telega4")}</span> <span className='max-[503px]:hidden'>{t("Telega5")}</span> <span className='max-[585px]:hidden'>{t("Telega6")}</span>
